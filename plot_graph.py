b0b7cd5355263e1ca44a9ae0075d4802d111b840
Shivam Nigam <shivam.nigam@ericsson.comwq>
2017-10-02 21:25:40 -0400

    Refactored and input points set to 30

--- plot_graph.py
+++ plot_graph.py
@@ -6,6 +6,7 @@ import numpy as np
 import time
 
 
+
 totalStart = time.time()
 date,bid,ask = np.loadtxt('/home/eshinig/comp/6731/project/python/GBPUSD/GBPUSD1d.txt', unpack=True,
                               delimiter=',',
@@ -18,28 +19,29 @@ patForRec = []
 
 
 def percentChange(startPoint, currentPoint):
-    return ((float(currentPoint) - startPoint)/abs(startPoint))*100.00
+    try:
+        x= ((float(currentPoint) - startPoint)/abs(startPoint))*100.00
+        if x == 0.0:
+            return 0.0000000000001
+        else:
+            return x
+    except:
+        return 0.000000001
+    
 
 def patternStorage():
     startTime = time.time();
-    x = len(avgLine) - 30
+    x = len(avgLine) - 60
 
-    y = 11
+    y = 31
     
     while (y<x):
         pattern = []
-        p1 = percentChange(avgLine[y-10], avgLine[y-9] )
-        p2 = percentChange(avgLine[y-10], avgLine[y-8] )
-        p3 = percentChange(avgLine[y-10], avgLine[y-7] )
-        p4 = percentChange(avgLine[y-10], avgLine[y-6] )
-        p5 = percentChange(avgLine[y-10], avgLine[y-5] )
-        p6 = percentChange(avgLine[y-10], avgLine[y-4] )
-        p7 = percentChange(avgLine[y-10], avgLine[y-3] )
-        p8 = percentChange(avgLine[y-10], avgLine[y-2] )
-        p9 = percentChange(avgLine[y-10], avgLine[y-1] )
-        p10 = percentChange(avgLine[y-10], avgLine[y] )
+        p = []
+        for i in xrange(30):
+            p.insert(i,percentChange(avgLine[y-30], avgLine[y-(29-i)]))
         
-        outcomeRange = avgLine[y+20:y+30]
+        outcomeRange = avgLine[y+30:y+60]
         currentPoint = avgLine[y]
         
         try:
@@ -49,22 +51,13 @@ def patternStorage():
             avgOutcome = 0
                         
         futureOutcome = percentChange(currentPoint,avgOutcome)
-        pattern.append(p1)
-        pattern.append(p2)
-        pattern.append(p3)
-        pattern.append(p4)
-        pattern.append(p5)
-        pattern.append(p6)
-        pattern.append(p7)
-        pattern.append(p8)
-        pattern.append(p9)
-        pattern.append(p10)
+
+        for i in xrange(30):
+            pattern.append(p[i])
         
         patterArr.append(pattern)
         performanceArr.append(futureOutcome)
-        #time.sleep(5555)
-        
-        y =y + 1
+        y = y + 1
 
     stopTime = time.time();
     print "patternArr: future % change" , len(patterArr)
@@ -74,53 +67,32 @@ def patternStorage():
    
 
 def currentPattern():
-    cp1 = percentChange(avgLine[-11],avgLine[-10])
-    cp2 = percentChange(avgLine[-11],avgLine[-9])
-    cp3 = percentChange(avgLine[-11],avgLine[-8])
-    cp4 = percentChange(avgLine[-11],avgLine[-7])
-    cp5 = percentChange(avgLine[-11],avgLine[-6])
-    cp6 = percentChange(avgLine[-11],avgLine[-5])
-    cp7 = percentChange(avgLine[-11],avgLine[-4])
-    cp8 = percentChange(avgLine[-11],avgLine[-3])
-    cp9 = percentChange(avgLine[-11],avgLine[-2])
-    cp10= percentChange(avgLine[-11],avgLine[-1])
+    cp = []
+    for i in xrange(30,0,-1):
+        cp.insert(30-i,percentChange(avgLine[-31],avgLine[-i]))
 
-    patForRec.append(cp1)
-    patForRec.append(cp2)
-    patForRec.append(cp3)
-    patForRec.append(cp4)
-    patForRec.append(cp5)
-    patForRec.append(cp6)
-    patForRec.append(cp7)
-    patForRec.append(cp8)
-    patForRec.append(cp9)
-    patForRec.append(cp10)
+
+    for i in xrange(30):
+        patForRec.append(cp[i])
 
     print "patternForRec" , patForRec
 
 
 def patternRecognition():
     for eachPattern in patterArr:
-        sim1 = 100-abs(percentChange(eachPattern[0],patForRec[0]))
-        sim2 = 100-abs(percentChange(eachPattern[1],patForRec[1]))
-        sim3 = 100-abs(percentChange(eachPattern[2],patForRec[2]))
-        sim4 = 100-abs(percentChange(eachPattern[3],patForRec[3]))
-        sim5 = 100-abs(percentChange(eachPattern[4],patForRec[4]))
-        sim6 = 100-abs(percentChange(eachPattern[5],patForRec[5]))
-        sim7 = 100-abs(percentChange(eachPattern[6],patForRec[6]))
-        sim8 = 100-abs(percentChange(eachPattern[7],patForRec[7]))
-        sim9 = 100-abs(percentChange(eachPattern[8],patForRec[8]))
-        sim10= 100-abs(percentChange(eachPattern[9],patForRec[9]))
+        similarPattern = []
+        for i in xrange(30):
+            similarPattern.insert(i,(100 - abs(percentChange(eachPattern[i],patForRec[i]))))
 
-        howSim = (sim1+sim2+sim3+sim4+sim5+sim6+sim7+sim8+sim9+sim10)/10.00
+        avgSimilarity = reduce(lambda x, y: x+y, similarPattern)/len(similarPattern)
 
-        if howSim > 70:
+        if avgSimilarity > 40:
             patIndex = patterArr.index(eachPattern)
             print '###############################'
             print "currentPattern[10]:" ,patForRec
             print "pastPattern[10]:" ,eachPattern
             print "predicted outcome", performanceArr[patIndex]
-            xp = [1,2,3,4,5,6,7,8,9,10]
+            xp = list(range(1,31))
             fig  = plt.figure()
             plt.plot(xp,patForRec)
             plt.plot(xp,eachPattern)
